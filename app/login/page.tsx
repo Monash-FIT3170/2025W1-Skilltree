a2738da79f39c96f975e9306bfad13c9839ba188
Nilly Hamidi <nillyhamidi@gmail.com>
2025-04-30 09:13:28 +1000

    Created initial login page routing - see on dev build with http://localhost:3000/login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,13 +1,16 @@
+export default function LogInPage() {
+  return <div>Log In Page</div>;
+}
+
 import LoginForm from "@/components/LoginForm";
 
 export default function LogInPage() {
-    return (  
-    <div className= "items-center">
+  return (
+    <div className="items-center">
       <h1 className="text-center text-2xl mt-10">Login</h1>
       <div className="flex items-center justify-center min-h-screen">
-        <LoginForm/>
+        <LoginForm />
       </div>
     </div>
-    );
-  }
-  
+  );
+}
