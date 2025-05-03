5d0c26bbe594da3e39874e4457ee84cbe3e67400
ANUJRANJAN0048 <aran0048@student.monash.edu>
2025-05-03 13:19:03 +1000

    homepage top section recommited again  due to conflicts

--- app/Homepage/homepage.tsx
+++ app/Homepage/homepage.tsx
@@ -4,14 +4,15 @@ import './homepage.css';
 const Homepage: React.FC = () => {
     return (
         <div className="main-content">
-            <div className="search-bar">
-                <input type="text" placeholder="Search Your Communities" className="input" />
+            <div className="top-section">
+                <div className="search-bar">
+                    <input type="text" placeholder="Search Your Communities" className="input" />
+                </div>
             </div>
-            <div className="community-section">
+            <div className="community-section"> 
                 <div className="profile-section">
                     <div className="profile-icon"></div>
-                    <span className="profile-name">Test</span>
-                    <div className="tooltip"> </div>
+                    <span className="profile-name">Test</span> 
                 </div>
             </div>
         </div>
