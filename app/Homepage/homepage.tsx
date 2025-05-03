fcdbb63ff8c0ac9dd326bc79331a49abd8157f35
rithikreddy680 <rsin0080@student.monash.edu>
2025-05-03 13:11:03 +1000

    Implement code changes to enhance functionality and improve

--- app/Homepage/homepage.tsx
+++ app/Homepage/homepage.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import './homepage.css'; 
+import './homepage.css';
 
 const Homepage: React.FC = () => {
     return (
         <div className="main-content">
             <div className="search-bar">
+                <img src="/app/Homepage/image.png" alt="Search Icon" /> 
                 <input type="text" placeholder="Search Your Communities" className="input" />
             </div>
-            <div className="community-section"> 
+            <div className="community-section">
                 <div className="profile-section">
                     <div className="profile-icon"></div>
-                    <span className="profile-name">Test</span> 
+                    <span className="profile-name">Test</span>
+                    <div className="tooltip"> </div>
                 </div>
             </div>
         </div>
