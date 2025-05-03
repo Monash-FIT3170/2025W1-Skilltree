0c08e83aaf060cccf2fa5dbef70e0bd3a52c7c46
ANUJRANJAN0048 <aran0048@student.monash.edu>
2025-05-03 13:22:04 +1000

    top section removed

--- app/Homepage/homepage.tsx
+++ app/Homepage/homepage.tsx
@@ -4,15 +4,14 @@ import './homepage.css';
 const Homepage: React.FC = () => {
     return (
         <div className="main-content">
-            <div className="top-section">
-                <div className="search-bar">
-                    <input type="text" placeholder="Search Your Communities" className="input" />
-                </div>
+            <div className="search-bar">
+                <input type="text" placeholder="Search Your Communities" className="input" />
             </div>
-            <div className="community-section"> 
+            <div className="community-section">
                 <div className="profile-section">
                     <div className="profile-icon"></div>
-                    <span className="profile-name">Test</span> 
+                    <span className="profile-name">Test</span>
+                    <div className="tooltip"> </div>
                 </div>
             </div>
         </div>
