b283391df36f53e434dc014311f721fc26c65f19
Aditya Tripathi <atri0048@student.monash.edu>
2025-05-12 13:48:15 +1000

    feat(Navbar): implement Navbar component with search and user avatar functionality; integrate into Providers

--- components/shared/providers.tsx
+++ components/shared/providers.tsx
@@ -3,24 +3,17 @@
 import {
   Sidebar,
   SidebarContent,
-  SidebarFooter,
   SidebarGroup,
   SidebarGroupContent,
-  SidebarGroupLabel,
-  SidebarHeader,
   SidebarInset,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
   SidebarProvider,
-  SidebarTrigger,
 } from "@/components/ui/sidebar";
 import Link from "next/link";
-import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { HomeIcon, AwardIcon, PlusIcon } from "lucide-react";
-import { Button } from "../ui/button";
-import { Input } from "../ui/input";
-import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip";
+import { Navbar } from "./Navbar";
 
 export const Providers = ({
   children,
@@ -31,36 +24,7 @@ export const Providers = ({
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <header className="border-b flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear">
-          <div className="w-full flex items-center justify-between gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
-
-            <Input
-              className="w-[300px] md:w-[400px] lg:w-[500px]"
-              placeholder="Search..."
-              type="text"
-            />
-
-            <div className="flex items-center justify-start gap-2">
-              <Tooltip>
-                <TooltipContent>User</TooltipContent>
-                <TooltipTrigger>
-                  <Avatar className="size-[32px]">
-                    <AvatarImage
-                      height={32}
-                      width={32}
-                      src="/images/avatar.png"
-                      alt="Avatar"
-                    />
-                    <AvatarFallback className="bg-primary text-primary-foreground">
-                      U
-                    </AvatarFallback>
-                  </Avatar>
-                </TooltipTrigger>
-              </Tooltip>
-            </div>
-          </div>
-        </header>
+        <Navbar />
         <main className="p-5">{children}</main>
       </SidebarInset>
     </SidebarProvider>
