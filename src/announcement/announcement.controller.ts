4b40faff87e7670a99cab7bbdaded77101a1af21
Steven Wenas <wenas.steven2@gmail.com>
2025-05-21 00:10:58 +1000

    add get one, get all and delete announcement

---
+++ src/announcement/announcement.controller.ts
@@ -0,0 +1,42 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { AnnouncementService } from './announcement.service';
+import { AnnouncementCreationDto } from './dto';
+import { User } from 'generated/prisma';
+import { JwtGuard } from 'src/guards';
+import { GetAnnouncementDto } from './dto/get-announcement.dto';
+import { GetUser } from 'src/decorator';
+
+@Controller('community')
+export class AnnouncementController {
+  constructor(private announcementService: AnnouncementService) {}
+
+  @Post('')
+  @UseGuards(JwtGuard)
+  createCommunity(@Body() dto: AnnouncementCreationDto, @GetUser() user: User) {
+    return this.announcementService.createAnnouncement(user, dto);
+  }
+
+  @Get('')
+  getAllAnnouncements() {
+    return this.announcementService.getAllAnnouncements();
+  }
+
+  @Get(':id')
+  getAnnouncementById(@Param() dto: GetAnnouncementDto) {
+    return this.announcementService.getAnnouncementById(dto);
+  }
+
+  @Delete(':id')
+  @UseGuards(JwtGuard)
+  deleteCommunity(@Param('id') id: string, @GetUser() user: User) {
+    return this.announcementService.deleteAnnouncement(id, user);
+  }
+}
--- src/announcement/announcement.service.ts
+++ src/announcement/announcement.service.ts
@@ -1,6 +1,8 @@
 import { HttpException, HttpStatus, BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AnnouncementCreationDto } from './dto/create-announcement.dto';
+import { GetAnnouncementDto } from './dto/get-announcement.dto';
+import { User } from 'generated/prisma';
 
 
 @Injectable()
@@ -8,8 +10,8 @@ export class AnnouncementService {
   constructor(private prisma: PrismaService) {}
 
   async createAnnouncement(
+    user: User,
     announcementCreationDto: AnnouncementCreationDto,
-    userId: string,
   ) {
     // Fetch the community by communityId
     const community = await this.prisma.community.findUnique({
@@ -22,7 +24,7 @@ export class AnnouncementService {
     }
 
     // throw error if creator of post is not creator of community
-    if (community.creatorId !== userId) {
+    if (community.creatorId !== user.id) {
       throw new BadRequestException('User is not the creator of this community');
     }
 
@@ -31,7 +33,7 @@ export class AnnouncementService {
       data: {
         text: announcementCreationDto.text,
         communityId: announcementCreationDto.communityId,
-        authorId: userId,  // The user creating the post must be the same as the creator
+        authorId: user.id,  // The user creating the post must be the same as the creator
       },
     });
 
@@ -39,4 +41,45 @@ export class AnnouncementService {
       message: announcement,
     };
   }
+    //might need to be change to all announcements where userID = announcement authorID
+    async getAllAnnouncements() {
+    const announcements = await this.prisma.post.findMany();
+
+    return {
+      message: announcements,
+    };
+  }
+    async getAnnouncementById(dto: GetAnnouncementDto) {
+      const announcement = await this.prisma.post.findUnique({
+        where: { id: dto.id },
+      });
+  
+      if (!announcement) {
+        throw new HttpException('Announcement not found', HttpStatus.NOT_FOUND);
+      }
+  
+      return { message: announcement };
+    }
+    async deleteAnnouncement(id: string, user: User) {
+        const announcement = await this.prisma.post.findUnique({
+          where: { id },
+        });
+    
+        if (!announcement) {
+          throw new HttpException('Announcement not found', HttpStatus.NOT_FOUND);
+        }
+    
+        if (announcement.authorId !== user.id) {
+          throw new HttpException(
+            'You are not authorized to delete this announcement',
+            HttpStatus.FORBIDDEN,
+          );
+        }
+    
+        await this.prisma.community.delete({
+          where: { id },
+        });
+    
+        return { message: 'Community deleted successfully' };
+      }
 }
