75e6eab7a757cb3a2009589d9e2ce875a5f02b4b
sara0012 <sara0012@student.monash.edu>
2025-05-25 06:07:35 +1000

    (m2) - added a search bar to nav bar

--- components/landingPage/NavBar.tsx
+++ components/landingPage/NavBar.tsx
@@ -40,6 +40,23 @@ const NavBar = () => {
             </li>
           ))}
         </ul>
+        {/* Search Bar */}
+      <div className="relative rounded-full bg-gray-200 px-4 py-2 flex items-center w-full max-w-md">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5 text-black mr-2"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-4.35-4.35M17 11a6 6 0 11-12 0 6 6 0 0112 0z" />
+        </svg>
+        <input
+          type="text"
+          placeholder="Search Skills, Communities...?"
+          className="bg-transparent outline-none w-full placeholder-black text-black"
+        />
+      </div>
         <div className="ml-auto flex space-x-4">
           <SignInButton />
           <LogInButton />
