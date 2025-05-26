1ade66d42eb06c42d6773c84fbd74b802a82eb3d
ANUJRANJAN0048 <aran0048@student.monash.edu>
2025-05-26 21:50:36 +1000

    Add SkillTreePage component with SkillTreeGraph integration

---
+++ app/(default)/communities/[id]/skilltree/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import SkillTreeGraph from "@/components/SkillTreeGraph";
+
+export default function SkillTreePage() {
+  return (
+    <div className="container mx-auto">
+      <SkillTreeGraph />
+    </div>
+  );
+}
