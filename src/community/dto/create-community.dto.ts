47a90bf30ea7c40afd89a5209da7bad6e7cde377
Steven <wenas.steven2@gmail.com>
2025-08-24 00:14:53 +1000

    community updates to fit prisma

--- src/community/dto/create-community.dto.ts
+++ src/community/dto/create-community.dto.ts
@@ -1,26 +1,24 @@
-import { IsNotEmpty, IsString, IsArray, IsOptional, IsInt, Min } from 'class-validator';
+import { IsNotEmpty, IsString, IsArray, IsOptional, Matches } from 'class-validator';
 
 export class CommunityCreationDto {
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  //accept client-provided slug as an option; otherwise generate from `name` variable server-side in the service
+  @IsOptional()
+  @IsString()
+  @Matches(/^[a-z0-9]+(?:-[a-z0-9]+)*$/, {
+    message: 'invalid slug',
+  })
+  slug?: string;
+
   @IsArray()
   @IsString({ each: true })
   @IsNotEmpty({ each: true })
   tags: string[];
 
-  @IsArray()
-  @IsString({ each: true })
-  @IsNotEmpty({ each: true })
-  skill: string[];
-
   @IsOptional()
   @IsString()
   description?: string;
-
-  @IsOptional()
-  @IsInt()
-  @Min(0)
-  communityExperience?: number;
 }
