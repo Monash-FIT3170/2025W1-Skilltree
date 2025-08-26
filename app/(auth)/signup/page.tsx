2986b1bebcce16fc13a4478f29eae3d979e2fbce
ANUJRANJAN0048 <aran0048@student.monash.edu>
2025-08-26 19:08:15 +1000

    changed path of skilltree logo

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -6,7 +6,7 @@ export default function SignupPage() {
   return (
     <div className="min-h-[calc(100vh-4rem)] flex flex-col items-center justify-center px-4">
       {/* logo + title like mock */}
-      <Image src="/skilltree-logo.png" alt="SkillTree" width={100} height={100} className="mb-3" />
+      <Image src="/images/skilltree-logo.jpg" alt="SkillTree" width={100} height={100} className="mb-3" />
       <h1 className="text-center text-xl font-semibold mb-6">Sign Up to SkillTree</h1>
       <SignupForm />
     </div>
