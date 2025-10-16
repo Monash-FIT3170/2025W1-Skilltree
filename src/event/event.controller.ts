fde2edae03c8598ad95791b857039700d2eac02b
wpad0001 <wpad0001@student.monash.edu>
2025-10-16 23:02:58 +1100

    add update event functionality and DTO for event updates

--- src/event/event.controller.ts
+++ src/event/event.controller.ts
@@ -3,6 +3,7 @@ import { EventService } from './event.service';
 import { JwtGuard } from '../_utils/guards/jwt.guard';
 import { CreateEventDto } from './dto/create-event.dto';
 import { DeleteEventDto } from './dto/delete-event.dto';
+import { UpdateEventDto } from './dto/update-event.dto';
 import { GetUser } from '../_utils/decorator/get-user.decorator';
 
 @Controller('event')
@@ -12,6 +13,11 @@ export class EventController {
 	@Get()
 	getAllEvents() {
 		return this.eventService.getAllEvents();
+	}
+
+	@Get(':id')
+	getEventById(@Param('id') eventId: string) {
+		return this.eventService.getEventById(eventId);
 	}
 
 	@UseGuards(JwtGuard)
@@ -40,5 +46,20 @@ export class EventController {
 	@Post(':id/leave')
 	leaveEvent(@Param('id') eventId: string, @GetUser() user: any) {
 		return this.eventService.leaveEvent(eventId, user.id);
+	}
+
+	@UseGuards(JwtGuard)
+	@Post(':id/update')
+	updateEvent(
+		@Param('id') eventId: string,
+		@Body() dto: UpdateEventDto,
+		@GetUser() user: any,
+	) {
+		return this.eventService.updateEvent(eventId, dto, user.id);
+	}
+
+	@Get(':id/users')
+	getUsers(@Param('id') eventId: string) {
+		return this.eventService.getEventUsers(eventId);
 	}
 }
